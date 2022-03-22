140b7d81caff60a748f6d87e788f259007ebf95c
iqamac <hyun77320@naver.com>
2022-03-22 09:18:10 +0900

    2022.03.22.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Children } from 'react'; //eslint-disable-line
+import MyComponent from './MyComponent'; //eslint-disable-line
+import Counter from './Counter'; //eslint-disable-line
+
+const App = () => {
+  return (
+    <Counter />
+  );
+};
+
+export default App;
