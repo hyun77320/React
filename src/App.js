b37be3edd17eec9d241121b2c0184e9fe6fd5021
iqamac <hyun77320@naver.com>
2022-03-23 20:05:45 +0900

    2022.03.23.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Children } from "react"; //eslint-disable-line
 import MyComponent from "./MyComponent"; //eslint-disable-line
 import Counter from "./Counter"; //eslint-disable-line
-import Say from "./Say";
+import Say from "./Say"; //eslint-disable-line
+import EventPractice from "./EventPractice"; //eslint-disable-line
 
 const App = () => {
-    return <Say />;
+    return <EventPractice />;
 };
 
 export default App;
