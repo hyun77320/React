4043c3f41c69550cda730efd95e2e33308ce2d44
iqamac <hyun77320@naver.com>
2022-03-26 21:58:59 +0900

    2022.03.26.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import MyComponent from "./MyComponent"; //eslint-disable-line
 import Counter from "./Counter"; //eslint-disable-line
 import Say from "./Say"; //eslint-disable-line
 import EventPractice from "./EventPractice"; //eslint-disable-line
+import ValidationSample from "./ValidationSample"; //eslint-disable-line
+import ScrollBox from "./ScrollBox";
 
 const App = () => {
-    return <EventPractice />;
+    return <ScrollBox />;
 };
 
 export default App;
