7ecf2084e67d12d2bb901a483924b4de8aadd3ad
iqamac <hyun77320@naver.com>
2022-03-26 16:25:16 +0900

    2022.03.26.

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -58,14 +58,25 @@
 import React, { useState } from 'react'; // 함수형
 
 const EventPractice = () => {
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
-    const onChangeUsername = e => setUsername(e.target.value);
-    const onChangeMessage = e => setMessage(e.target.value);
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const {username, message} = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
+    };
+    
     const onClick = () => {
         alert(username + ':' + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username: '',
+            message: ''
+        })
     };
     const onKeyPress = e => {
         if (e.key === 'Enter') {
@@ -81,14 +92,14 @@ const EventPractice = () => {
                 name='username'
                 placeholder='사용자명'
                 value={username}
-                onChange={onChangeUsername}                    
+                onChange={onChange}                    
             />
             <input
                 type='text'
                 name='message'
                 placeholder='아무거나 입력해 보세요'
                 value={message}
-                onChange={onChangeMessage}
+                onChange={onChange}
                 onKeyPress={onKeyPress}
             />
             <button onClick={onClick}>확인</button>
