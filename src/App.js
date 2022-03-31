58e49266734ebb337d588e5de88da87cb831863f
iqamac <hyun77320@naver.com>
2022-03-31 14:08:17 +0900

    2022.03.31.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,32 @@ import EventPractice from "./EventPractice"; //eslint-disable-line
 import ValidationSample from "./ValidationSample"; //eslint-disable-line
 import ScrollBox from "./ScrollBox"; //eslint-disable-line
 import IterationSample from "./IterationSample"; //eslint-disable-line
+import LifeCycleSample from "./LifeCycleSample"; //eslint-disable-line
+import ErrorBoundary from "./ErrorBoundary"; //eslint-disable-line
+
+function getRandomcolor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component{
+    state = {
+        color: '#000000'
+    }
+    
+    handleClick = () => {
+        this.setState({
+            color: getRandomcolor()
+        });
+    }
+
     render() {
         return (
-            <IterationSample />
+            <div>
+                <button onClick={this.handleClick}>랜덤 색상</button>
+                <ErrorBoundary>
+                    <LifeCycleSample color={this.state.color}/>
+                </ErrorBoundary>
+            </div>
         );
     }
 };
